557dafbe8d403e61203dc32841abd5eaade88ae3
Jeeves <guydoodlesdev@gmail.com>
2024-09-05 22:22:11 -0600

    add build script

--- flake.nix
+++ flake.nix
@@ -9,13 +9,24 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem(system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      nodejs = pkgs.nodejs_20;
     in {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ nodejs_20 ];
+        packages = [ nodejs ];
         shellHook = ''
           export NO_UPDATE_NOTIFIER=1
           npm i
         '';
       };
+
+      packages.default = pkgs.buildNpmPackage {
+        name = "littlebiglogic";
+        src = ./.;
+        npmDepsHash = "sha256-g48xblHNi28hLdQVhSjW4r8Vp9e3KuEBRVZKgXzFC0Y=";
+        inherit nodejs;
+        installPhase = "mv dist $out";
+      };
+
+      hydraJobs.build = self.packages.${system}.default;
     });
 }
