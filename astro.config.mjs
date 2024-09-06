1d244f5b94fdece822c04d7c3e18af608fc4dad1
Jeeves <guydoodlesdev@gmail.com>
2024-09-06 15:45:19 -0600

    set site url to wiki.littlebigcomputer.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://littlebiglogic.jeevio.xyz',
+  site: 'https://wiki.littlebigcomputer.com',
   integrations: [mdx()],
 });
